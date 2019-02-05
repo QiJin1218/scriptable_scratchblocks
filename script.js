4b67a45e028a9852cffad2527193a90cc2c8845c
Qi Jin <qijin@Qis-MacBook-Pro.local>
2019-02-04 23:20:53 -0600

    Commit

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+  	headless: false,
+  });
   const page = await browser.newPage();
-  await page.goto('http://scratchblocks.github.io/#?style=scratch3&script=');
-  await page.screenshot({path: 'google.png'});
-
-  await browser.close();
+  const PNG_SELECTOR = '#export-png';
+  await page.goto('http://scratchblocks.github.io/#?style=scratch3&script=test');
+  await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: './scratchblocks_png/' });
+  await page.click(PNG_SELECTOR);
+  setTimeout(() => { browser.close(); }, 50);
+  // await browser.close();
 })();
